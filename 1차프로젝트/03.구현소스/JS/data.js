6ec1ed0eeb9e4376402336a38ec296a0bbbccff7
nons9633 <novall9633@naver.com>
2023-08-30 16:50:43 +0900

    img 변경

---
+++ 1차프로젝트/03.구현소스/JS/data.js
@@ -0,0 +1,6 @@
+const data={
+    "ATLAS":{"이미지":"c","색상":"Purple•Teal•Navy","설명":"ATLAS만의 새롭게 디자인된 코어와<br>Heavy Oil에서도 힘을 잃지 않는 Formula 1 Solid 커버의 조합"},
+    "ENVY TOUR PEARL":{"이미지":"h","색상":"Chrome","설명":"최상과 최상의 만남<br>Obsession Tour 코어와 HK22-Envy Pearl 커버"},
+    "BW LEGEND SOLID":{"이미지":"h","색상":"Black","설명":"강력한 힘을 가진 Envy Solid CFI+ 커버와<br>증명된 Gas Mask 코어의 결합"},
+    "THE C-9 KNIT BOA":{"이미지":"d","색상":"Black•Blue","설명":"고급 니트 소재의 포근한 착화감<br>유니크한 디자인의 최고급 볼링화"}
+}
