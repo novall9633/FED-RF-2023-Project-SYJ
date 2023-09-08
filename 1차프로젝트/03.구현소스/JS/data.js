d63c12b1ea0c05fce06561751aaf793e0e71ba49
nons9633 <novall9633@naver.com>
2023-09-08 18:32:17 +0900

    js 추가

--- 1차프로젝트/03.구현소스/JS/data.js
+++ 1차프로젝트/03.구현소스/JS/data.js
@@ -1,6 +1,6 @@
-const data={
-    "ATLAS":{"볼이미지":"16","이미지":"c","색상":"Purple•Teal•Navy","설명":"ATLAS만의 새롭게 디자인된 코어와<br>Heavy Oil에서도 힘을 잃지 않는 Formula 1 Solid 커버의 조합"},
-    "ENVY TOUR PEARL":{"볼이미지":"14","이미지":"h","색상":"Chrome","설명":"최상과 최상의 만남<br>Obsession Tour 코어와 HK22-Envy Pearl 커버"},
-    "BW LEGEND SOLID":{"볼이미지":"15","이미지":"h","색상":"Black","설명":"강력한 힘을 가진 Envy Solid CFI+ 커버와<br>증명된 Gas Mask 코어의 결합"},
-    "THE C-9 KNIT BOA":{"볼이미지":"13","이미지":"d","색상":"Black•Blue","설명":"고급 니트 소재의 포근한 착화감<br>유니크한 디자인의 최고급 볼링화"}
-}
+const liveData=[
+    {ballName:"ATLAS",bImg:"16",img:"c",color:"Purple•Teal•Navy",info:"ATLAS만의 새롭게 디자인된 코어와<br>Heavy Oil에서도 힘을 잃지 않는 Formula 1 Solid 커버의 조합"},
+    {ballName:"ENVY TOUR PEARL",bImg:"14",img:"h",color:"Chrome",info:"최상과 최상의 만남<br>Obsession Tour 코어와 HK22-Envy Pearl 커버"},
+    {ballName:"BW LEGEND SOLID",bImg:"15",img:"h",color:"Black",info:"강력한 힘을 가진 Envy Solid CFI+ 커버와<br>증명된 Gas Mask 코어의 결합"},
+    {ballName:"THE C-9 KNIT BOA",bImg:"13",img:"d",color:"Black•Blue",info:"고급 니트 소재의 포근한 착화감<br>유니크한 디자인의 최고급 볼링화"}
+]
--- 1차프로젝트/03.구현소스/JS/main.js
+++ 1차프로젝트/03.구현소스/JS/main.js
@@ -2,80 +2,193 @@ let pg_num = 0;
 let sts_wheel = 0;
 let total_pg = 0;
 let ele_page;
-let pos_start=0, pos_end=0;
+let pos_start = 0,
+  pos_end = 0;
+let hcode = "";
+let icode = "";
+let clickSts = 0;
+const TIME_SLIDE = 400;
 
-setTimeout(()=>{window.scrollTo(0,0)},500);
+setTimeout(() => {
+  window.scrollTo(0, 0);
+}, 500);
 
-window.addEventListener('touchstart',touchStart);
-window.addEventListener('touchend',touchEnd);
-window.addEventListener('wheel',wheelFn);
-window.addEventListener('DOMContentLoaded',loadFn);
+window.addEventListener("touchstart", touchStart);
+window.addEventListener("touchend", touchEnd);
+window.addEventListener("wheel", wheelFn);
+window.addEventListener("DOMContentLoaded", loadFn);
 
-const qs = x => document.querySelector(x);
-const qsa = x => document.querySelectorAll(x);
-const addEvt =(ele,evt,fn) => ele.addEventListener(evt,fn);
+const domFn = {
+  // 요소선택함수 ////////
+  qs: (x) => document.querySelector(x),
+  qsEl: (el, x) => el.querySelector(x),
+  qsa: (x) => document.querySelectorAll(x),
+  qsaEl: (el, x) => el.querySelectorAll(x),
 
-function loadFn(){
-    ele_page = qsa('.page');
+  // 이벤트셋팅함수
+  addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
+}; /////// domFn 객체 /////////////
+
+function loadFn() {
+    ele_page = domFn.qsa(".page");
     total_pg = ele_page.length;
+    // 이동버튼
+    const abtn = domFn.qsa('.abtn');
+    //슬라이드 이미지//////////////////////
+    let swipWrap = domFn.qs(".swip-wrap");
+  let swipNav = domFn.qs(".swip-nav");
+  icode += `<ol class="indic">`;
+  liveData.forEach((ele,idx) => {
+      if(idx==0){
+          hcode += `    
+    <div class="slide-img onSlide">
+        <div class="slide-img-info">
+            <p class="manu-comp">
+            <img src="./imgs/main_visual_icon_${ele.img}.png" />
+            </p>
+            <h1 class="ball-name info">${ele.ballName}</h1>
+            <h2 class="ball-color info">${ele.color}</h2>
+            <h3 class="ball-intro info">
+            ${ele.info}
+            </h3>
+            
+            <div class="btn-more info">
+            <a href="#"> 
+            MORE 
+            <img src="imgs/pin.png" alt="볼링핀">
+            </a>
+            </div>
+            </div>
+            <div class="bgimg">
+            <div class="dtimg">
+            <img src="./imgs/main_visual${ele.bImg}.jpg" alt="${ele.ballName}" />
+            </div>
+            </div>
+    </div>`;
+    icode += `<li class="on">
+    <img src="./imgs/dot1.png" alt="흰색">
+    <img src="./imgs/dot2.png" alt="회색">
+    </li>`;
+}else{
+    hcode
+    +=
+    `<div class="slide-img">
+    <div class="slide-img-info">
+    <p class="manu-comp">
+    <img src="./imgs/main_visual_icon_${ele.img}.png" />
+    </p>
+    <h1 class="ball-name info">${ele.ballName}</h1>
+    <h2 class="ball-color info">${ele.color}</h2>
+    <h3 class="ball-intro info">
+    ${ele.info}
+    </h3>
     
+    <div class="btn-more info">
+    <a href="#"> 
+    MORE 
+    <img src="imgs/pin.png" alt="볼링핀">
+    </a>
+    </div>
+    </div>
+    <div class="bgimg">
+    <div class="dtimg">
+    <img src="./imgs/main_visual${ele.bImg}.jpg" alt="${ele.ballName}" />
+    </div>
+    </div>
+    </div>`;
+    
+    icode += `<li>
+    <img src="./imgs/dot1.png" alt="흰색">
+    <img src="./imgs/dot2.png" alt="회색">
+    </li>`;
+    }
+});
+icode += `</ol>`;
+swipWrap.innerHTML = hcode;
+swipNav.innerHTML = icode;
+//슬라이드 이미지//////////////////////
 
-}/////////loadFn함수////////////////////////////
+const indic = domFn.qsa('.indic li');
+swipWrap.querySelectorAll('.slide-img').forEach((ele,idx)=>{
+    ele.setAttribute('data-seq',idx);
+});
+swipNav.querySelectorAll('li').forEach((ele,idx)=>{
+    ele.setAttribute('data-seq',idx);
+});
+abtn.forEach(ele=>{
+    domFn.addEvt(ele,'click',goSlide);
+});
+
+function goSlide(){
+    if(clickSts) return;
+    clickSts =1;
+    setTimeout(()=>{
+        clickSts=0;
+    },TIME_SLIDE);
+    
+    let isRight = this.classList.contains('swip-next');
+    let eachOne = domFn.qsa('.slide-img');
+    let nowSeq = swipWrap.querySelectorAll('.slide-img')[isRight?1:0].getAttribute('data-seq')
+    console.log(nowSeq);
+    indic.forEach((ele,idx)=>{
+        if(idx==nowSeq) ele.classList.add('on');
+        else ele.classList.remove('on');
+    });
+  }
+} /////////loadFn함수////////////////////////////
 
 /******************************************************************** 
     함수명 : wheelFn
     기능 : 마우스 휠 작동시 페이지이동
 ********************************************************************/
-function wheelFn(e){
-    if(sts_wheel) return;
-    sts_wheel = 1;
-    setTimeout(()=>{sts_wheel=0},700); 
+function wheelFn(e) {
+  if (sts_wheel) return;
+  sts_wheel = 1;
+  setTimeout(() => {
+    sts_wheel = 0;
+  }, 700);
 
-    let delta = e.wheelDelta;
+  let delta = e.wheelDelta;
 
-    if(delta<0){
-        pg_num++;
-    }else if(delta>0){
-        pg_num--;
-    }
+  if (delta < 0) {
+    pg_num++;
+  } else if (delta > 0) {
+    pg_num--;
+  }
 
-    if(pg_num < 0) pg_num=0;
-    if(pg_num == total_pg) pg_num=total_pg-1;
+  if (pg_num < 0) pg_num = 0;
+  if (pg_num == total_pg) pg_num = total_pg - 1;
 
-    console.log("delta:",delta,"/pg_num:",pg_num);  
-    window.scrollTo(0,window.innerHeight*pg_num);
-    
+  console.log("delta:", delta, "/pg_num:", pg_num);
+  window.scrollTo(0, window.innerHeight * pg_num);
 } // wheelFn 함수////////////////////////////
 
-
-
 //  터치시작 이벤트 호출 함수 /////////////
-function touchStart(e){ 
-    pos_start = e.touches[0].screenY;
+function touchStart(e) {
+  pos_start = e.touches[0].screenY;
 } //touchStart함수////////////////////
 
 //  터치끝 이벤트 호출 함수 /////////////
-function touchEnd(e){ //e는 이벤트 전달변수
-    pos_end = e.changedTouches[0].screenY;
+function touchEnd(e) {
+  //e는 이벤트 전달변수
+  pos_end = e.changedTouches[0].screenY;
 
-    let result = pos_start - pos_end;
+  let result = pos_start - pos_end;
 
-    if(result==0) return;
-    movePage(result>0?1:0);
-
+  if (result == 0) return;
+  movePage(result > 0 ? 1 : 0);
 } //touchEnd함수////////////////////
 
 //// 이벤트 처리함수 : 화면이동 /////
-function movePage(dir){ 
+function movePage(dir) {
+  if (dir) {
+    pg_num++;
+  } else if (!dir) {
+    pg_num--;
+  }
 
-    if(dir){
-        pg_num++;
-    }else if(!dir){
-        pg_num--;
-    }
+  if (pg_num < 0) pg_num = 0;
+  if (pg_num == total_pg) pg_num = total_pg - 1;
 
-    if(pg_num < 0) pg_num=0;
-    if(pg_num == total_pg) pg_num=total_pg-1;
-
-    window.scrollTo(0,ele_page[pg_num].offsetTop);
-}/////movePage함수 /////////////
+  window.scrollTo(0, ele_page[pg_num].offsetTop);
+} /////movePage함수 /////////////
