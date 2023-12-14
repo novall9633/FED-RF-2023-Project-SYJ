75b71ad8a78b48379c8ff9e36781c1ecf55782ac
nons9633 <novall9633@naver.com>
2023-12-14 18:18:57 +0900

    Update VisiImg.jsx

--- 2차프로젝트/03.구현소스/nexis/src/components/modules/VisiImg.jsx
+++ 2차프로젝트/03.구현소스/nexis/src/components/modules/VisiImg.jsx
@@ -1,5 +1,4 @@
 import { subBusiness } from "../data/subBusiness";
-import $ from "jquery";
 import "../../css/subBusiness.css";
 import { Link } from "react-router-dom";
 import { nCon } from "./nContext";
@@ -81,61 +80,9 @@ export function VisiImg(props) {
                     </div>
                 </div>
 {/*                                                                      */}
-                <div className="cont_bx">
-                    <div className="quality">
-                        <div className="bx_img">
-                            <em></em>
-                            <p></p>
-                        </div>
-                        <div className="bx_txt inner cf">
-                            <div className="txt_big">
-                                <p className=" text_op left">
-                                    <span>Quality</span>
-                                </p>
-                                <p className="text_img">
-                                    
-                                </p>
-                            </div>
-                            <div className="txt_cont">
-                                <p className="title">
-                                    <span>Domestic Edition</span>
-                                </p>
-                                <p className="txt">
-                                    보이지 않는 곳까지 완벽을 추구하는 품질 제일주의를 바탕으로 <br />
-                                    대단위 공동주택, 호텔, 리조트 등에 주방 및 수납 가구를 납품하고 있습니다.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="content service right active">
-                        <div className="bx_img">
-                            <p></p>
-                        </div>
-                        <div className="bx_txt inner cf">
-                            <div className="txt_big">
-                                <p className="text_op right">
-                                    <span>Best</span>
-                                </p>
-                                <p className="text_img">
-                                   
-                                </p>
-                            </div>
-
-                            <div className="txt_cont">
-                                <p className="title">Pursuit of Perfection</p>
-                                <p className="txt">
-                                    건설사와 시행사의 요구를 바탕으로 최적의 설계, 최고의 품질, 완벽한 시공과 AS를
-                                    약속합니다.
-                                </p>
-
-                                <p className="btn_area">
-                                    <a href="/file/2019넥시스_국내프로젝트자료.pdf" target="_blank" className="btn_s1">
-                                        <span>모델하우스 현황</span>
-                                    </a>
-                                </p>
-                            </div>
-                        </div>
+                <div className="subBs-first">
+                    <div className="subBs-st-img">
+                        
                     </div>
                 </div>
             </div>
