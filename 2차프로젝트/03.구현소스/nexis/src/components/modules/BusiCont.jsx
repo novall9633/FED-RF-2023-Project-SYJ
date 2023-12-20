0ff68a9038d0e53ce1db52554c67de69ffa5d29c
nons9633 <novall9633@naver.com>
2023-12-20 18:33:38 +0900

    Update BusiCont.jsx

--- 2차프로젝트/03.구현소스/nexis/src/components/modules/BusiCont.jsx
+++ 2차프로젝트/03.구현소스/nexis/src/components/modules/BusiCont.jsx
@@ -31,29 +31,30 @@ export function BusiCont(props) {
     };
     return (
         <>
-            {
-                <div className="subBs-first">
-                <div className="subBs-st1">
-                    <div className="subBs-st-cont">
-                        <div className="subBs-st-img greyImg"></div>
-                        <div className="subBs-st-img contImg">
-                            {/* <img src={process.env.PUBLIC_URL + v.src} alt="" /> */}
-                        </div>
-                    </div>
-                    <div className="subBs-st-txt">
-                        {/* <h2>{v.txt1}</h2> */}
-                    </div>
-                </div>
-                <div className="subBs-st2">
-                    <div className="subBs-st2-cont">
-                        <div className="subBs-st2-svg"></div>
-                        <div className="subBs-st2-txt">
-                            {/* <h2>{v.sub}</h2> */}
-                            {/* <h4>{v.txt2}</h4> */}
-                        </div>
-                    </div>
-                </div>
-            </div>}
+            {subBusiCont[props.cat].map((v) => {
+                <div>{v.src}</div>
+                // <div className="subBs-first">
+                //     <div className="subBs-st1">
+                //         <div className="subBs-st-cont">
+                //             <div className="subBs-st-img greyImg"></div>
+                //             <div className="subBs-st-img contImg">
+                //                 {/* <img src={process.env.PUBLIC_URL + v.src} alt="" /> */}
+                //             </div>
+                //         </div>
+                //         <div className="subBs-st-txt">{/* <h2>{v.txt1}</h2> */}</div>
+                //     </div>
+                //     <div className="subBs-st2">
+                //         <div className="subBs-st2-cont">
+                //             <div className="subBs-st2-svg"></div>
+                //             <div className="subBs-st2-txt">
+                //                 {/* <h2>{v.sub}</h2> */}
+                //                 {/* <h4>{v.txt2}</h4> */}
+                //             </div>
+                //         </div>
+                //     </div>
+                // </div>
+
+            })}
         </>
     );
 }
