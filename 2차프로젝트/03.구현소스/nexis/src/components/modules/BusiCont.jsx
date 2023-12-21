e41ef2941a67c12dbf112de7f88fae8468609c54
nons9633 <novall9633@naver.com>
2023-12-21 11:15:31 +0900

    svg 추가

--- 2차프로젝트/03.구현소스/nexis/src/components/modules/BusiCont.jsx
+++ 2차프로젝트/03.구현소스/nexis/src/components/modules/BusiCont.jsx
@@ -1,60 +1,46 @@
 import { subBusiCont } from "../data/subBusiCont";
 export function BusiCont(props) {
-    const makeCode = (data) => {
-        return data.map((v, i) => {
-            console.log(v.src);
-            <>
+    let code = subBusiCont[props.cat][props.idx];
+
+    const makeArr = () => {
+        let arr = code.svg;
+        let seqNum = 1;
+        const hcode = [];
+        for (let i = 0; i < arr.length; i++) {
+            hcode[i] = <path className="pa1" d={code.svg[i]} key={i} />;
+            seqNum++;
+        }
+        return hcode;
+    };
+
+    return (
+        <>
+            {
                 <div className="subBs-first">
                     <div className="subBs-st1">
                         <div className="subBs-st-cont">
                             <div className="subBs-st-img greyImg"></div>
                             <div className="subBs-st-img contImg">
-                                <img src={process.env.PUBLIC_URL + v.src} alt="" />
+                                <img src={process.env.PUBLIC_URL + code.src} alt="" />
                             </div>
                         </div>
                         <div className="subBs-st-txt">
-                            <h2>{v.txt1}</h2>
+                            <h2>{code.txt1}</h2>
                         </div>
                     </div>
                     <div className="subBs-st2">
                         <div className="subBs-st2-cont">
-                            <div className="subBs-st2-svg"></div>
+                            <div className="subBs-st2-svg">
+                                <svg>{makeArr()}</svg>
+                            </div>
                             <div className="subBs-st2-txt">
-                                <h2>{v.sub}</h2>
-                                <h4>{v.txt2}</h4>
+                                <h2>{code.sub}</h2>
+                                <h4>{code.txt2}</h4>
                             </div>
                         </div>
                     </div>
                 </div>
-            </>;
-        });
-    };
-    return (
-        <>
-            {subBusiCont[props.cat].map((v) => {
-                <div>{v.src}</div>
-                // <div className="subBs-first">
-                //     <div className="subBs-st1">
-                //         <div className="subBs-st-cont">
-                //             <div className="subBs-st-img greyImg"></div>
-                //             <div className="subBs-st-img contImg">
-                //                 {/* <img src={process.env.PUBLIC_URL + v.src} alt="" /> */}
-                //             </div>
-                //         </div>
-                //         <div className="subBs-st-txt">{/* <h2>{v.txt1}</h2> */}</div>
-                //     </div>
-                //     <div className="subBs-st2">
-                //         <div className="subBs-st2-cont">
-                //             <div className="subBs-st2-svg"></div>
-                //             <div className="subBs-st2-txt">
-                //                 {/* <h2>{v.sub}</h2> */}
-                //                 {/* <h4>{v.txt2}</h4> */}
-                //             </div>
-                //         </div>
-                //     </div>
-                // </div>
-
-            })}
+            }
         </>
     );
 }
--- 2차프로젝트/03.구현소스/nexis/src/components/pages/Business.jsx
+++ 2차프로젝트/03.구현소스/nexis/src/components/pages/Business.jsx
@@ -1,13 +1,18 @@
 import { BusiCont } from "../modules/BusiCont";
 import { VisiImg } from "../modules/VisiImg";
+import { subBusiCont } from "../data/subBusiCont";
 
-export function Business(props){
-    return(
+export function Business(props) {
+    return (
         <>
-        <div id="sub-busi" className="business">
-            <VisiImg cat={props.cat} />
-            <BusiCont cat={props.cat} />
-        </div>
+            <div id="sub-busi" className="business">
+                <VisiImg cat={props.cat} />
+                <BusiCont cat={props.cat} idx={0} />
+                {
+                    props.cat !== "wall" &&
+                    <BusiCont cat={props.cat} idx={1} />
+                }
+            </div>
         </>
-    )
+    );
 }
