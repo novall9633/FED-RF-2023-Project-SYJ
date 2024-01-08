2b02408ab795d4f6e208fa68089e90b1f9ae0c46
nons9633 <novall9633@naver.com>
2024-01-08 18:35:16 +0900

    미디어 쿼리 중간 점검

--- 2차프로젝트/03.구현소스/nexis/src/components/pages/Main.jsx
+++ 2차프로젝트/03.구현소스/nexis/src/components/pages/Main.jsx
@@ -17,27 +17,34 @@ export function Main(){
 
     useEffect(()=>{
         let mounted = true;
-        let main2 = $(".main2");
-        let trends = $(".trends-wrap");
-        let trendsImg = $(".trends-img");
-        let requestTxt = $(".request-project-req");
-        let requestImg = $(".request-project-img");
-        let golf=$(".golf");
-        let golfMem=$(".golfMem");
-        const mouseMoveM = () =>{
-            scrollFn("top",main2,1/4);
-            scrollFn("right30",trends,1/3);
-            scrollFn("scale",trendsImg,2/3);
-            scrollFn("right30",requestTxt,2/3);
-            scrollFn("left30",requestImg,2/3);
-            scrollFn("top",golf,4/5);
-            scrollFn("top",golfMem,4/5);
-        }
-        window.addEventListener('wheel',()=>{
-            if(mounted){
-                mouseMoveM();
+        if(matchMedia("screen and (min-width: 1024px)").matches)
+        {
+            console.log("1024이상");
+            let main2 = $(".main2");
+            let trends = $(".trends-wrap");
+            let trendsImg = $(".trends-img");
+            let requestTxt = $(".request-project-req");
+            let requestImg = $(".request-project-img");
+            let golf=$(".golf");
+            let golfMem=$(".golfMem");
+            const mouseMoveM = () =>{
+                scrollFn("top",main2,1/4);
+                scrollFn("right30",trends,1/3);
+                scrollFn("scale",trendsImg,2/3);
+                scrollFn("right30",requestTxt,2/3);
+                scrollFn("left30",requestImg,2/3);
+                scrollFn("top",golf,4/5);
+                scrollFn("top",golfMem,4/5);
             }
-        });
+            window.addEventListener('wheel',()=>{
+                if(mounted){
+                    mouseMoveM();
+                }
+            });
+        }
+        else{
+            console.log("1024이하");
+        }
         return () => {
             mounted=false;
         };
