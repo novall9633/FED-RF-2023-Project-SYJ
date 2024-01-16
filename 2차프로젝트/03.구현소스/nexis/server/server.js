c61ad816c0b4e48574b17876be63748921458b2b
nons9633 <novall9633@naver.com>
2024-01-17 08:56:48 +0900

    Update server.js

--- 2차프로젝트/03.구현소스/nexis/server/server.js
+++ 2차프로젝트/03.구현소스/nexis/server/server.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var app = express();
 var client_id = process.env.REACT_APP_CLIENT_ID; //개발자센터에서 발급받은 Client ID
+// REACT_APP_CLIENT_ID = 'hwl1zg9wp_QipovESODT',
 var client_secret = process.env.REACT_APP_CLIENT_SECRET; //개발자센터에서 발급받은 Client Secret
+// REACT_APP_CLIENT_SECRET = "9DletTgwZr"
 var code = "0";
 app.get("/captcha/nkey", function (req, res) {
     var api_url = "https://openapi.naver.com/v1/captcha/nkey?code=" + code;
